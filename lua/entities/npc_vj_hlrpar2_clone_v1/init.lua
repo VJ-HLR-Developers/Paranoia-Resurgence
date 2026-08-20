c01d05a4efe2d7516c8547e46fa717baaedfee59
Darkborn <darkandrai1987@gmail.com>
2026-08-20 02:45:29 -0400

    Update init.lua

--- lua/entities/npc_vj_hlrpar2_clone_v1/init.lua
+++ lua/entities/npc_vj_hlrpar2_clone_v1/init.lua
@@ -3,3 +3,9 @@ AddCSLuaFile("shared.lua")
 include("shared.lua")
 
 ENT.Model = "models/vj_parr/par2/v1/monster_clonsoldier.mdl"
+
+local math_random = math.random
+---------------------------------------------------------------------------------------------------------------------------------------------
+function ENT:Soldier_Init()
+    self:SetBodygroup(1, math_random(0, 2))
+end
