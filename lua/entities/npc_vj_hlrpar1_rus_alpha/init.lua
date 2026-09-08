7d0a06d6ddb785b7141eda687a56bf3441c3eab9
Darkborn <darkandrai1987@gmail.com>
2026-09-08 15:15:14 -0400

    Static corpse failsafe

--- lua/entities/npc_vj_hlrpar1_rus_alpha/init.lua
+++ lua/entities/npc_vj_hlrpar1_rus_alpha/init.lua
@@ -761,6 +761,7 @@ function ENT:OnDeath(dmginfo, hitgroup, status)
         if GetConVar("vj_hlr1_corpse_static"):GetInt() == 1 && VJ_CVAR_AI_ENABLED && self.HasDeathAnimation then
             self.DeathAnimationDecreaseLengthAmount = -1
             self.DeathCorpseEntityClass = "prop_vj_animatable"
+            VJ.HLR_StaticCorpseCheck(self)
         end
         if hitgroup == HITGROUP_HEAD then
             VJ.EmitSound(self, "vj_parr/par1/shared/headshot.wav", 75, 100)
