4182455d3c223f0dd444df0e97c6bbbbc6b1c8de
Darkborn <darkandrai1987@gmail.com>
2026-09-21 13:27:02 -0400

    Minor updates

--- lua/entities/npc_vj_hlrpar1_worker_early/init.lua
+++ lua/entities/npc_vj_hlrpar1_worker_early/init.lua
@@ -18,8 +18,8 @@ function ENT:Civilian_Init()
         self.Civilian_CanHurtWalk = true
         self.Weapon_UnarmedBehavior = false
         self:SetBodygroup(3, math_random(0, 1))
-        self:SetSkin(math_random(0, 1))
     end
+    self:SetSkin(math_random(0, 1))
     self:SetBodygroup(1, math_random(0, 2))
     self:SetBodygroup(2, math_random(0, 1))
     if myMDL != "models/vj_parr/par1/early/npc_worker_old.mdl" then self:SetBodygroup(3, math_random(0, 2)) end
